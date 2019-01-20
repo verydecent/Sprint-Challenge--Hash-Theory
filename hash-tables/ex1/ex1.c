73e538a9f2e727e999e7aa7fbd62fe1d78bcf5ea
verydecent <wnjhwng@gmail.com>
2019-01-19 21:08:31 -0800

    ex1 pass

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -3,12 +3,34 @@
 #include "hashtable.h"
 #include "ex1.h"
 
+Answer *create_answer(int i1, int i2)
+{
+  Answer *answer = malloc(sizeof(Answer));
+  answer->index_1 = i1;
+  answer->index_2 = i2;
+  return answer;
+}
+
+
+
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
-  HashTable *ht = create_hash_table(16);
-
-  // YOUR CODE HERE
-
+  HashTable *ht = create_hash_table(16); 
+  for (int i = 0; i < length; i++)
+  {
+    hash_table_insert(ht,  weights[i],  i);
+  }
+  for (int i = 0; i < length; i++)
+  {
+    int search = hash_table_retrieve(ht, limit - weights[i]);       
+    if (search != -1)
+    {      
+      Answer *answer = create_answer(search, i);
+      destroy_hash_table(ht);
+      return answer;
+    }
+  }
+  destroy_hash_table(ht);
   return NULL;
 }
 
