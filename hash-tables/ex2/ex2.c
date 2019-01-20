bcb2809c9805a2936d0003d72a0ce21784dafdc2
verydecent <wnjhwng@gmail.com>
2019-01-19 21:09:51 -0800

    ex2 pass

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -9,7 +9,17 @@ char **reconstruct_trip(Ticket **tickets, int length)
   HashTable *ht = create_hash_table(16);
   char **route = malloc(length * sizeof(char *));
 
-  // YOUR CODE HERE
+  for (int i = 0; i < length; i++)
+  {
+    hash_table_insert(ht, tickets[i]->source, tickets[i]->destination);
+  }
+  char *curr_ticket = "NONE";  
+  for (int i = 0; i < length; i++)
+  {
+    route[i] = hash_table_retrieve(ht, curr_ticket);
+    curr_ticket = route[i];    
+  } 
+  destroy_hash_table(ht);
 
   return route;
 }
